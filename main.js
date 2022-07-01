f4dfc0dd31477e3d9715dc9c13108a1aee8a696a
Johan Bell <belljohan@hotmail.com>
2022-07-01 09:02:00 +0100

    Vue App created, mounted and displayed on the DOM

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return{
+            product: 'Boots',
+            description: 'It is used to work in differents areas'
+        }
+    }
+})
