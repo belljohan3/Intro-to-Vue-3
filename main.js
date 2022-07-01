4ab12398f449d92f7abe8e8dfc08b750d17537f5
Johan Bell <belljohan@hotmail.com>
2022-07-01 12:05:35 +0100

    Event handling, attribute binding, conditional rendering

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 const app = Vue.createApp({
     data() {
         return{
-            product: 'Boots',
-            description: 'It is used to work in differents areas'
+            cart:0,
+            product: 'Socks',
+            image: '/assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/',
+            inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 1234, color: 'green', image: '/assets/images/socks_green.jpg'},
+                {id: 5678, color: 'blue', image: '/assets/images/socks_blue.jpg'},
+            ],
+            sizes: ['38','40', '42', '43']
+       }
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        removeFromCart() {
+            this.cart -= 1
+        },
+
+        removeAll() {
+            this.cart = 0
+        },
+
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
