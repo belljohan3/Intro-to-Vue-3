897595c55561e76bf088cd22589bf5a5d5f77881
Johan Bell <belljohan@hotmail.com>
2022-07-04 15:52:01 +0100

    Review form added

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
     methods: {
-        updateCart() {
-            this.cart += 1
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+                if (index > -1) {
+                    this.cart.splice(index, 1)
+                }
         }
     }
 })
