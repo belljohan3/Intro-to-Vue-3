8d1ec378b5d90691aa84991db7ce8a7b6f28441a
Johan Bell <belljohan@hotmail.com>
2022-07-08 21:42:19 +0100

    Dynamic Route

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="event-card">
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-  </div>
+  <router-link 
+  class="event-link"
+  :to="{ name: 'EventDetails', params: { id: event.id } }">
+    <div class="event-card">
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -23,6 +27,10 @@ export default {
   border: 1px solid #39495c;
   margin-bottom: 18px;
 }
+.event-link {
+        color: #2c3e50;
+        text-decoration: none;
+    }
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
